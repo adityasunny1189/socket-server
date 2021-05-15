1fd63b49cb09b690bb831af0668a8b001732128e
adityasunny1189 <adityapathak1189@gmail.com>
2021-05-15 20:22:34 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const io = require("socket.io")(process.env.PORT, {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"],
+    },
+  });
+  io.on("connection", (socket) => {
+    console.log("connected");
+  
+    socket.on("get-codeId", (codeId,user_name) => {
+      socket.join(codeId);
+      socket.on("disconnect", () => {
+        console.log(disconnected:-${codeId});
+      });
+  
+      socket.broadcast.to(codeId).emit("recieve-name", user_name);
+  
+      socket.on("send-changes", (code, language) => {
+        socket.broadcast.to(codeId).emit("recieve-changes", code, language);
+      });
+    });
+  });
