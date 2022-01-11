236b0622d01130e87f5d50a77744f650777a648b
Aditya Pathak <46691394+adityasunny1189@users.noreply.github.com>
2022-01-11 18:48:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,22 +1,26 @@
-const io = require("socket.io")(process.env.PORT, {
-    cors: {
-      origin: "*",
-      methods: ["GET", "POST"],
-    },
-  });
-  io.on("connection", (socket) => {
-    console.log("connected");
+// const io = require("socket.io")(process.env.PORT, {
+//     cors: {
+//       origin: "*",
+//       methods: ["GET", "POST"],
+//     },
+//   });
+//   io.on("connection", (socket) => {
+//     console.log("connected");
   
-    socket.on("get-codeId", (codeId,user_name) => {
-      socket.join(codeId);
-      socket.on("disconnect", () => {
-        console.log(`disconnected:-${codeId}`);
-      });
+//     socket.on("get-codeId", (codeId,user_name) => {
+//       socket.join(codeId);
+//       socket.on("disconnect", () => {
+//         console.log(`disconnected:-${codeId}`);
+//       });
   
-      socket.broadcast.to(codeId).emit("recieve-name", user_name);
+//       socket.broadcast.to(codeId).emit("recieve-name", user_name);
   
-       socket.on("send-changes", (code, language) => {
-        socket.broadcast.to(codeId).emit("recieve-changes", code, language);
-      });
-    });
-  });
+//        socket.on("send-changes", (code, language) => {
+//         socket.broadcast.to(codeId).emit("recieve-changes", code, language);
+//       });
+//     });
+//   });
+
+function Hello() {
+    console.log("Hello");
+}
